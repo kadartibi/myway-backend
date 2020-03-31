4700508c136b4e2b6e19a0448a2581d2c0d26bfc
LaszloNagy85 <n.laszlo.uk@gmail.com>
2020-03-31 14:12:15 +0200

    refactor

--- src/main/java/com/codecool/myway/security/SecurityConfig.java
+++ src/main/java/com/codecool/myway/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/auth/login").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().permitAll();
 
     }
 
