b33fb17225a1402880702126431ad12cb02b4d6c
LaszloNagy85 <n.laszlo.uk@gmail.com>
2020-03-31 09:08:53 +0200

    refactor

--- src/main/java/com/codecool/myway/repositories/PlannedDayRepository.java
+++ src/main/java/com/codecool/myway/repositories/PlannedDayRepository.java
@@ -2,6 +2,8 @@ package com.codecool.myway.repositories;
 
 import com.codecool.myway.entities.PlannedDayEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PlannedDayRepository extends JpaRepository<PlannedDayEntity, Long> {
+
 }
--- src/main/java/com/codecool/myway/repositories/TripRepository.java
+++ src/main/java/com/codecool/myway/repositories/TripRepository.java
@@ -1,7 +1,9 @@
 package com.codecool.myway.repositories;
 
+import com.codecool.myway.entities.PlannedDayEntity;
 import com.codecool.myway.entities.TripEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,4 +12,8 @@ public interface TripRepository extends JpaRepository<TripEntity, Long> {
     List<TripEntity> findTop5ByOrderByRatingDesc();
 
     List<TripEntity> findAllByOrderByIdDesc();
+
+//
+//    @Query("select p.plannedDays from TripEntity p  ")
+//    List<PlannedDayEntity> getPlannedDays();
 }
