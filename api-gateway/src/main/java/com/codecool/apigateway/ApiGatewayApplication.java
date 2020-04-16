dde921cfb860125163e984ab5765c0a4da64127e
LaszloNagy85 <n.laszlo.uk@gmail.com>
2020-04-16 10:12:00 +0200

    disable security login on eureka

--- api-gateway/src/main/java/com/codecool/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/codecool/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,11 @@ package com.codecool.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableZuulProxy
 @EnableEurekaClient
 public class ApiGatewayApplication {
